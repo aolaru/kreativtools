87eb26584f78f927569e7ea162822293759c1d0e
andreiolaru <andrei.olaru@gmail.com>
2026-05-14 11:50:00 +0300

    Configure live PayPal checkout link

--- payments-config.js
+++ payments-config.js
@@ -1,7 +1,7 @@
 window.KREATIV_PAYMENTS = window.KREATIV_PAYMENTS || {
   workflows: {
     provider: 'PayPal',
-    checkoutUrl: '',
+    checkoutUrl: 'https://www.paypal.com/ncp/payment/B9QW497VKU7C8',
     restoreContact: '/contact/',
     successUrl: '/workflows/success/',
   },
